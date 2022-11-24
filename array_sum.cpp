f5ea655f51543300ab1ef1bd8cfbce18ab3c16b2
Shravan Revanna <94772842+githubhosting@users.noreply.github.com>
2022-11-24 10:41:56 +0530

    Create array_sum.cpp

---
+++ array_sum.cpp
@@ -0,0 +1,17 @@
+//Program to add the numbers in an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size, sum = 0;
+    cout << "Enter the size of the Array: ";
+    cin >> size;
+    int myNumbers[size];
+    for(int i=0; i<size; i++){
+        cin >> myNumbers[i];
+        sum += myNumbers[i];
+    }
+    cout << "The sum of the Array: " << sum;
+    
+    return 0;
+}
